c5def50ce2cb719409e1498bf69b53959c3fc3ff
FranchiniMae <fmae.mercado@gmail.com>
2016-04-11 21:46:35 -0700

    Adds one parallax

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('linked');
+
+// var ypos,image;
+// function parallax() {
+// 	ypos = window.pageYOffset;
+// 	image = document.getElementById('image');
+// 	image.style.top = ypos * .4 + 'px';
+// }
+// window.addEventListener('scroll', parallax);
+
+	var documentEl = $(document),
+	parallaxBg = $('div.parallax-bg');
+
+	documentEl.on('scroll', function () {
+		var currScrollPos = documentEl.scrollTop();
+		parallaxBg.css('background-position', '0 ' + -currScrollPos/4 + 'px');
+	});
+
+
+
