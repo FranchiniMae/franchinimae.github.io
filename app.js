13256295edc61aec2b3b2090f83c3e1a1fb71cd7
FranchiniMae <fmae.mercado@gmail.com>
2016-04-16 12:00:48 -0700

    Changes carousel arrows

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 console.log('linked');
 
 $(document).ready(function(){
-$('.projectinfo').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-});
+	$('.projectinfo').slick({
+	  dots: false,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+  	nextArrow: '<i class="slickright fa fa-angle-double-right"></i>',
+  	prevArrow: '<i class="slickleft fa fa-angle-double-left"></i>',
+  	appendArrows: $('.arrowdiv') 
+	});
 });
 	
 
+// $('.slider-container').slick({ 
+// 	slide: '.slider-item', 
+// 	arrows: true, 
+// 	dots: true, 
+// 	prevArrow: '<span class="slick-prev"></span>', 
+// 	nextArrow: '<span class="slick-next"></span>', 
+// 	appendArrows: '.slider-container .slider-nav', 
+// 	appendDots: '.slider-container .slider-nav' });
 //   var documentEl = $(document),
 // 	parallaxBg = $('div.parallax-bg');
 
